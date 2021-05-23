52e2fb98db818d9c8224244cf4a6f4c03d9291f5
Kaleb Pattawi <72418535+kpattawi@users.noreply.github.com>
2021-05-23 15:31:52 -0700

    putting new fuzzy

--- EPMultipleSims_v4_editting/EPMultipleSims_v4_generated/Controller/src/main/java/org/webgme/guest/controller/Controller.java
+++ EPMultipleSims_v4_editting/EPMultipleSims_v4_generated/Controller/src/main/java/org/webgme/guest/controller/Controller.java
@@ -340,9 +340,13 @@ public class Controller extends ControllerBase {
                 p=0;
                 System.out.println("p"+String.valueOf(p));
             }
-            heatTemps[i]=Double.parseDouble(futureIndoorTemp[p]);
+            // heatTemps[i]=Double.parseDouble(futureIndoorTemp[p]);
             System.out.println("heatTemp"+String.valueOf(heatTemps[i]));
-            coolTemps[i]=30.2;
+            
+            // I think if we set these as a band 
+            heatTemps[i]=Double.parseDouble(futureIndoorTemp[p])-0.5;
+            coolTemps[i]=Double.parseDouble(futureIndoorTemp[p])+0.5;
+            coolTemps[i] = 30.2; // do this for now to avoid turning on AC
             System.out.println("coolTemp: "+String.valueOf(coolTemps[i]));
             p=p+1;
             System.out.println("p"+String.valueOf(p));
@@ -366,28 +370,45 @@ public class Controller extends ControllerBase {
             // End Adaptive Setpoint Control
 
             // 0.5 degree fuzzy control (this oscillates indoor temp)
+            double max_cool_temp = 30.2; 
+            double min_heat_temp = 18.9; 
             double OFFSET = 0.6; // need to change slightly higher/lower so E+ doesnt have issues
 
+            // Determine minimum and maximum temperatures allowed (we can probably print this from optimization code too)
+            if (outTemps[i]<=10){
+                min_heat_temp =18.9;
+                max_cool_temp =22.9;
+            }else if (outTemps[i]>=33.5){
+                min_heat_temp =26.2;
+                max_cool_temp =30.2;
+            }else {
+                min_heat_temp = 0.31*outTemps[i] + 17.8-2;
+                max_cool_temp = 0.31*outTemps[i] + 17.8+2;
+            }
+
+            // Now set maximum cool and minimum heats:
+            if (coolTemps[i]>=max_cool_temp){
+                coolTemps[i]=max_cool_temp;
+            }
+            if (heatTemps[i]<=min_heat_temp){
+                heatTemps[i]=min_heat_temp;
+            }
+
             // For Cooling 1 degree under Cooling setpoint:
-            if (zoneTemps[i] >= coolTemps[i]-.1){
+            if (zoneTemps[i] >= coolTemps[i]-.1){ // first check if going to exit maximum band
                 fuzzy_cool = -1;
             } else if (zoneTemps[i] <= coolTemps[i]-1.1){
-                fuzzy_cool = +1;
+                fuzzy_cool = 1;
             }
             coolTemps[i] = coolTemps[i] - 0.6 +fuzzy_cool*OFFSET;   // -0.6 so that oscillates 0.1-1.1 degree under cooling setpoint
 
             // For Heating 1 degree under Heating setpoint:
-            if (zoneTemps[i] >= heatTemps[i]+1.1){
+            if (zoneTemps[i] <= heatTemps[i]+.1){ // first check if going to exit minimum band
+                fuzzy_heat = 1;
+            } else if (zoneTemps[i] >= heatTemps[i]+1.1){
                 fuzzy_heat = -1;
-            } else if (zoneTemps[i] <= heatTemps[i]+.1){
-                fuzzy_heat = +1;
             }
             heatTemps[i] = heatTemps[i] + 0.6 +fuzzy_heat*OFFSET;  // +0.6 so that oscillates 0.1-1.1 degree above heating setpoint
-
-            System.out.println("heatTemps[i] = "+i+ heatTemps[i] );
-            System.out.println("coolTemps[i] = "+i+ coolTemps[i] );
-          }
-          // End fuzzy control
           //-------------------------------------------------------------------------------------------------
 
 
